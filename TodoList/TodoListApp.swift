e31e30acc22454601ef14781456c9cac7c22e1e9
Mohammed H. Mahmood <mohammed.hamidf18@komar.edu.iq>
2023-08-04 23:35:22 +0300

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Mohammed Mahmood on 04/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
